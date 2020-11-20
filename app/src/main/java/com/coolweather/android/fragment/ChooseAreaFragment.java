7523130962cde5e1ed01ed89cc852101073dcd5d
godwei <2382043923@qq.com>
2020-11-20 16:52:51 +0800

    从服务器上请求并更新数据到ui

--- app/src/main/java/com/coolweather/android/fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/fragment/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.coolweather.android.fragment;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.coolweather.android.R;
+import com.coolweather.android.WeatherActivity;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
@@ -78,6 +80,12 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
@@ -174,7 +182,6 @@ public class ChooseAreaFragment extends Fragment {
                     public void run() {
                         closeProgressDialog();
                         Toast.makeText(getContext(), "加载失败了啊o(╥﹏╥)o", Toast.LENGTH_SHORT).show();
-
                     }
                 });
             }
